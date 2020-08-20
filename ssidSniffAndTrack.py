2a1fe9793577f8238e85d54b824660992b5b16dc
Matt <58438294+Zeneeth@users.noreply.github.com>
2020-08-20 05:38:52 -0400

    added distance calculation and error checking

--- ssidSniffAndTrack.py
+++ ssidSniffAndTrack.py
@@ -7,8 +7,8 @@ import time
 
 wigleuser = #PUT WIGLE API NAME HERE
 wiglepass = #PUT WIGLE API TOKEN HERE
-currentLat = 0
-currentLon = 0
+currentLat = 51.509865
+currentLon = -0.118092
 macssid = {}
 ssidlocations = {}
 welcome = [
@@ -23,29 +23,50 @@ welcome = [
 "-----------------------------------------------------------------------------"]
 
 def findCurrentLocation():
-    resp = requests.get(url="http://bot.whatismyipaddress.com/").text
-    location = requests.get(url="http://ip-api.com/json/" + resp).json()
-    currentLat = location['lat']
-    currentLon = location['lon']
+    try:
+        resp = requests.get(url="http://bot.whatismyipaddress.com/").text
+        location = requests.get(url="http://ip-api.com/json/" + resp).json()
+        currentLat = location['lat']
+        currentLon = location['lon']
+    except:
+        print("Could not get location using the internet so defaulted to central london")
+
+def calculateDistance(lat1, lon1, lat2, lon2):
+    R = 6378100
+    φ1 = lat1 * math.pi/180
+    φ2 = lat2 * math.pi/180
+    Δφ = (lat2-lat1) * math.pi/180
+    Δλ = (lon2-lon1) * math.pi/180
+
+    a = math.sin(Δφ/2) * math.sin(Δφ/2) + math.cos(φ1) * math.cos(φ2) * math.sin(Δλ/2) * math.sin(Δλ/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    d = R * c
+
+    return d
 
 def findNetwork(ssid):
-    payload = payload = {'ssid': ssid}
-    resp = requests.get(url='https://api.wigle.net/api/v2/network/search', params=payload, auth=(wigleuser, wiglepass)).json()
+    try:
+        payload = payload = {'ssid': ssid}
+        resp = requests.get(url='https://api.wigle.net/api/v2/network/search', params=payload, auth=(wigleuser, wiglepass)).json()
 
-    if resp['success']:
-        if resp['totalResults'] == 0:
-            #print("Did not find a network with that name")
-            return "none found"
-        elif resp['totalResults'] > 1:
-            print("Found several networks with that name, the network could be at the following locations")
-            for i in range(0, resp['totalResults']):
-                print(resp['results'][i]['trilat'], resp['results'][i]['trilong'])
-            return "lots"
+        if resp['success']:
+            if resp['totalResults'] == 0:
+                return "none found"
+            elif resp['totalResults'] > 1:
+                minDistance = calculateDistance(currentLat, currentLon, resp['results'][0]['trilat'], resp['results'][0]['trilong'])
+                minIndex = 0
+                for i in range(1, resp['totalResults']):
+                    if minDistance > calculateDistance(currentLat, currentLon, resp['results'][i]['trilat'], resp['results'][i]['trilong']):
+                        minDistance = calculateDistance(currentLat, currentLon, resp['results'][i]['trilat'], resp['results'][i]['trilong'])
+                        minIndex = i
+                return str(resp['results'][minIndex]['trilat'], resp['results'][minIndex]['trilong'])
+            else:
+                return str(resp['results'][0]['trilat'], resp['results'][0]['trilong'])
         else:
-            print("Network is likely to be at:", resp['results'][0]['trilat'], resp['results'][0]['trilong'])
-            return str(resp['results'][0]['trilat'], resp['results'][0]['trilong'])
-    else:
-        return "Response failed:", resp['message']
+            return "Response failed: " + resp['message']
+    except:
+        return "Error fetching data from wigle"
 
 def packetCheck(pkt):
     if pkt.type == 0 and pkt.subtype == 4:
